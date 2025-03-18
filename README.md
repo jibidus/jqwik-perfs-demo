534ced6a2e275db819c36d1882177dec8c9d252a
Jibidus <jibidus@gmail.com>
2025-03-18 09:14:30 +0100

    Demonstrate the performance degradation

---
+++ README.md
@@ -0,0 +1,10 @@
+Demo project which illustrates a performance degradation in jqwik 1.9.0 (same with 1.9.1 and 1.9.2).
+
+See [this test](src/test/java/org/example/Test.kt).
+
+In my laptop, this test last **1mn47s with jqwik 1.9.0**, but **3s with jqwik 1.8.5**.
+
+This test involves 3 elements:
+* duplicated arbitrary providers in different domains (there is no duration difference without this)
+* these providers uses `combine()` kotlin DSL with a single arbitrary (there is no duration difference with `Double.any().map{}` instead)
+* usage of `anyForSubtypeOf<>()` (the duration difference is smaller with `Arbitraries.oneOf(anyForType<SubClass1>().enableRecursion(), …)`)
--- src/test/java/org/example/Test.kt
+++ src/test/java/org/example/Test.kt
@@ -1,41 +1,51 @@
 package org.example
 
-import net.jqwik.api.Arbitraries
 import net.jqwik.api.Arbitrary
+import net.jqwik.api.Combinators
 import net.jqwik.api.ForAll
 import net.jqwik.api.Property
 import net.jqwik.api.Provide
-import net.jqwik.api.constraints.UseType
 import net.jqwik.api.domains.Domain
-import net.jqwik.api.domains.DomainContext
 import net.jqwik.api.domains.DomainContextBase
 import net.jqwik.kotlin.api.any
 import net.jqwik.kotlin.api.anyForSubtypeOf
-import net.jqwik.kotlin.api.anyForType
+import net.jqwik.kotlin.api.combine
 
 @Domain(MyDomain::class)
-@Domain(DomainContext.Global::class)
+@Domain(AnotherDomain::class)
 class Test {
 
     @Property
-    fun test(@ForAll model: @UseType Model) {
-
+    fun test(@ForAll model: Model) {
     }
 }
-
 
 class MyDomain : DomainContextBase() {
     @Provide
     fun model(): Arbitrary<Model> = anyForSubtypeOf<Model>()
-//    fun model(): Arbitrary<Model> = Arbitraries.just(Model1(1))
 
     @Provide
-    fun subModel(): Arbitrary<SubModel> = Double.any().between(0.0, 1_000_000.0).map { SubModel(it) }
+    fun attribute() = combine {
+        val value by Double.any()
+        combineAs {
+            Attr(value)
+        }
+    }
+}
+
+class AnotherDomain : DomainContextBase() {
+    @Provide
+    fun attribute() = combine {
+        val value by Double.any()
+        combineAs {
+            Attr(value)
+        }
+    }
 }
 
 sealed interface Model
 
-data class Model1(val attr1: SubModel) : Model
-data class Model2(val attr1: SubModel,val attr2: SubModel?) : Model
+data class Model1(val attr1: Attr) : Model
+data class Model2(val attr1: Attr) : Model
 
-data class SubModel(val int: Double)
+data class Attr(val value: Double)
