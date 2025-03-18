a91c8635e9ae4ca09f053573cc5db223dedf3f1b
Jibidus <jibidus@gmail.com>
2025-03-18 09:14:30 +0100

    demonstrate the performance issue with anyForSubtypeOf()

---
+++ README.md
@@ -0,0 +1,5 @@
+Demo project which illustrate a performance degradation in jqwik 1.9.0, including 1.9.1 and 1.9.2.
+
+See [this test](src/test/java/org/example/Test.kt).
+
+In my laptop, this test last **1s with jqwik 1.9.0**, but **0.4s with jqwik 1.8.5**.
--- src/test/java/org/example/Test.kt
+++ src/test/java/org/example/Test.kt
@@ -1,6 +1,5 @@
 package org.example
 
-import net.jqwik.api.Arbitraries
 import net.jqwik.api.Arbitrary
 import net.jqwik.api.ForAll
 import net.jqwik.api.Property
@@ -9,9 +8,7 @@ import net.jqwik.api.constraints.UseType
 import net.jqwik.api.domains.Domain
 import net.jqwik.api.domains.DomainContext
 import net.jqwik.api.domains.DomainContextBase
-import net.jqwik.kotlin.api.any
 import net.jqwik.kotlin.api.anyForSubtypeOf
-import net.jqwik.kotlin.api.anyForType
 
 @Domain(MyDomain::class)
 @Domain(DomainContext.Global::class)
@@ -19,23 +16,21 @@ class Test {
 
     @Property
     fun test(@ForAll model: @UseType Model) {
-
     }
 }
 
-
 class MyDomain : DomainContextBase() {
     @Provide
-    fun model(): Arbitrary<Model> = anyForSubtypeOf<Model>()
-//    fun model(): Arbitrary<Model> = Arbitraries.just(Model1(1))
+    fun model(): Arbitrary<Model> {
+        return anyForSubtypeOf<Model>()
+    }
 
-    @Provide
-    fun subModel(): Arbitrary<SubModel> = Double.any().between(0.0, 1_000_000.0).map { SubModel(it) }
+    // Workaround
+//    fun model(): Arbitrary<Model> = Arbitraries.oneOf(anyForType<Model1>(), anyForType<Model2>(), anyForType<Model3>())
 }
 
 sealed interface Model
 
-data class Model1(val attr1: SubModel) : Model
-data class Model2(val attr1: SubModel,val attr2: SubModel?) : Model
-
-data class SubModel(val int: Double)
+data class Model1(val attr1: Int, val attr2: Int, val attr3: Int, val attr4: Int) : Model
+data class Model2(val attr1: Int, val attr2: Int, val attr3: Int, val attr4: Int) : Model
+data class Model3(val attr1: Int, val attr2: Int, val attr3: Int, val attr4: Int) : Model
