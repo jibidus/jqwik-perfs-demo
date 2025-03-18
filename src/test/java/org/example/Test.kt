93b1506c94945848f734d49771bfacfaf587ba5c
Jibidus <jibidus@gmail.com>
2025-03-18 09:04:52 +0100

    continue to search

--- src/test/java/org/example/Test.kt
+++ src/test/java/org/example/Test.kt
@@ -9,7 +9,9 @@ import net.jqwik.api.constraints.UseType
 import net.jqwik.api.domains.Domain
 import net.jqwik.api.domains.DomainContext
 import net.jqwik.api.domains.DomainContextBase
+import net.jqwik.kotlin.api.any
 import net.jqwik.kotlin.api.anyForSubtypeOf
+import net.jqwik.kotlin.api.anyForType
 
 @Domain(MyDomain::class)
 @Domain(DomainContext.Global::class)
@@ -26,9 +28,14 @@ class MyDomain : DomainContextBase() {
     @Provide
     fun model(): Arbitrary<Model> = anyForSubtypeOf<Model>()
 //    fun model(): Arbitrary<Model> = Arbitraries.just(Model1(1))
+
+    @Provide
+    fun subModel(): Arbitrary<SubModel> = Double.any().between(0.0, 1_000_000.0).map { SubModel(it) }
 }
 
 sealed interface Model
 
-data class Model1(val int: Int) : Model
-data class Model2(val int: String) : Model
+data class Model1(val attr1: SubModel) : Model
+data class Model2(val attr1: SubModel,val attr2: SubModel?) : Model
+
+data class SubModel(val int: Double)
